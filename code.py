6eded864253511f86f02c7291223532832240a4c
ZARPLAY <millerkil156@gmail.com>
2024-06-07 09:41:30 +0300

    code.py

--- code.py
+++ code.py
@@ -1,4 +1,15 @@
-a = input("цифра 1: ")
-b = input("цифра 2: ")
-c = int(a) + int(b)  # Преобразуйте введенные значения в целые числа и выполните сложение
-print(c)
+clear
+a = float(input("цифра 1: "))  # Преобразуем введенное значение в число с плавающей запятой
+b = float(input("цифра 2: "))  # Преобразуем введенное значение в число с плавающей запятой
+
+# Выполняем математические операции
+addition = a + b
+subtraction = a - b
+multiplication = a * b
+division = a / b
+
+# Выводим результаты
+print("Сложение:", addition)
+print("Вычитание:", subtraction)
+print("Умножение:", multiplication)
+print("Деление:", division)
